6d3b16c138281ea856da71c8ae78934d74279ff0
ibrahimnd2000 <ibrahimnd2000@gmail.com>
2022-12-25 15:43:32 +0500

    Added renderedItem

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const Accordion = (props) => {
-  return <div>Accordion</div>;
+const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => {
+    return (
+      <div key={item.title}>
+        <div className='title active'>
+          <i className='dropdown icon' />
+          {item.title}
+        </div>
+        <div className='content active'>
+          <p>{item.content}</p>
+        </div>
+      </div>
+    );
+  });
+  return <div>{renderedItems}</div>;
 };
 
 export default Accordion;
